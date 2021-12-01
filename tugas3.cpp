03bed2b0c50394c8152257ca319305c41eaa32ec
dedyalfaris <dedyalfaris.da41@gmail.com>
2021-12-01 23:53:39 +0700

    Tugas 3 C++
    Berikut saya lampirkan tugas 3 c++ saya kepada niomic

---
+++ tugas3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    int a, b, c;
+    cout<<"Nilai Variabel A : ";
+    cin>>a;
+    cout<<"Nilai Variabel B : ";
+    cin>>b;
+    c=a*b;
+    cout<<"Hasil Perkalian Kedua Variabel Adalah : "<<c<<endl;
+
+}
